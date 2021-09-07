413fa15482306f48f0adb0ef8759e692213fd554
PratikGarai <pratikgarai0208@gmail.com>
2021-09-07 07:27:19 +0530

    Logging

--- bot.py
+++ bot.py
@@ -1,15 +1,20 @@
 import discord
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger("my-logger")
+logger.setLevel(logging.INFO)
 
 token = open("token.txt", "r").read()
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")
+    logger.info(f"We have logged in as {client.user}")
 
 @client.event
 async def on_message(message):
-    print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
+    logger.info(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
 
     if "hello" in message.content.lower():
         await message.channel.send("Hi!")
