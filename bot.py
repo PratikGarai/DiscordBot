8d917e7c7651f97e92b70195273f1894596fd1d4
PratikGarai <pratikgarai0208@gmail.com>
2021-09-07 09:50:54 +0530

    New commands

--- bot.py
+++ bot.py
@@ -1,11 +1,31 @@
 import discord
 from discord.ext import commands
 
+intents = discord.Intents.all()
 token = open("token.txt", "r").read()
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
+@bot.command(name="mcount")
+async def member_count(ctx : commands.Context):
+    await ctx.send(f"Member count : {ctx.message.guild.member_count}")
+
+
+@bot.command(name="mstats")
+async def member_stats(ctx : commands.Context):
+    online = 0
+    offline = 0
+    idle = 0
+
+    members = ctx.message.guild.members
+    for m in members :
+        if str(m.status) == "online":
+            online += 1
+        elif str(m.status) == "offline":
+            offline += 1
+        else:
+            idle += 1
+
+    await ctx.send(
+        f"```Members : {len(members)}\nOnline members : {online}\nOffline members : {offline}\nIdle/Hidden : {idle}```")
 
 bot.run(token)
